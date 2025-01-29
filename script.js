9c92e845fa0659cf1eaf07edc3aaf256e6bc6e9b
ABHISHEK BIRADAR <162307955+seekernothing@users.noreply.github.com>
2025-01-29 11:25:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function (){
+	return `${this.make}, ${this.model}`
+}
+
+function SportsCar(make, model, topSpeed) {
+	 Car.call(this, make, model); // Inheriting properties from Car constructor
+	this.topSpeed = topSpeed;
+}
+
+SportsCar.prototype.getTopSpeed= function(){
+	return `${this.topSpeed}`
+}
+
 
 // Do not change the code below
 window.Car = Car;
